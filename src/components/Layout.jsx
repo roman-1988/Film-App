57be574d3e4914063378e0ddabbc478def77f295
Roman <roman-1@inbox.ru>
2023-05-15 21:38:07 +0300

    Добавил и подключил в компаненте Layout логотип. А также добавил стили в нем.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,8 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles"
 import { CssBaseline } from "@mui/material"
+import { Link } from "react-router-dom"
+import logoImg from "../assets/logo.png"
+import { styled } from "@mui/system"
 
 const darkTheme = createTheme({
     palette: {
@@ -7,10 +10,33 @@ const darkTheme = createTheme({
     }
 })
 
+const Img = styled("img")({
+    marginLeft: "auto",
+    marginRight: "auto",
+    display: "block",
+    width: 500,
+    maxWidth: "100%"
+})
+
+const LayoutWrapper = styled("div")(({theme}) => ({
+    margin: 24,
+    width: "auto",
+    [theme.breakpoints.up("lg")] : {
+        marginLeft: "auto",
+        marginRight: "auto",
+        width: theme.breakpoints.values.lg
+    }
+}))
+
 const Layout = ({ children }) => {
     return (
         <ThemeProvider theme={darkTheme}>
             <CssBaseline />
+            <LayoutWrapper>
+                <Link to="/">
+                    <Img src={logoImg} alt="The films db" />
+                </Link>
+            </LayoutWrapper>
             {children}
         </ThemeProvider>
     )
